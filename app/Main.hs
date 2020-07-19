a81265a0aa4e6a186d9763a801d372806678eddd
archikapiswe <archikumari97@gmail.com>
2020-07-19 23:19:59 +0530

    fix build issues
    modify Readme

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
 module Main where
 
-import Lib
+
+import qualified HedTypes as HT
+import qualified EditorObj as EO
+import qualified TextObj as TO
+import qualified Event as E
+import qualified UI as UI
+import Control.Lens
+import Control.Exception
+import Data.Default (def)
+import Brick
+import System.Environment (getArgs)
 
 main :: IO ()
-main = someFunc
+main = do
+  f <- getArgs
+  let fname = case f of
+                [name] -> name
+                _      -> ""
+  hed fname
+  return ()
+
+app :: App HT.EditorObj () HT.Name
+app = App
+  { appDraw = UI.drawEditorObj
+  , appChooseCursor = UI.appCursor
+  , appHandleEvent = E.handleEditor
+  , appStartEvent = return
+  , appAttrMap = const UI.theMap
+  }
+
+hed f = do
+ file <-safeLoadFile f
+ let contents = case file of 
+        Left _ -> ""
+        Right x -> x
+ defaultMain app (EO.emptyEO & (HT.editTextObjL.HT.nameL .~ HT.EditPad).(HT.editTextObjL .~ (TO.makeTO contents)).(HT.infoL .~ f))
+
+safeLoadFile :: FilePath -> IO (Either IOException String)
+safeLoadFile f = (Right <$> readFile f) `catch` (\ e -> pure (Left e) )
